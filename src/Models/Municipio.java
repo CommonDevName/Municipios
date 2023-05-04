5ee2c1c83a7675ce9a1d6283a0431100d30521bf
Jeshua A. G. Guillén <132015069+CommonDevName@users.noreply.github.com>
2023-05-03 19:56:19 -0600

    Added the source code

---
+++ src/Models/Municipio.java
@@ -0,0 +1,18 @@
+package Models;
+
+/**
+ *
+ * @author uncor
+ */
+public class Municipio {
+    private int id;
+    private String nombre;
+
+    public Municipio(int id, String nombre){
+    this.id = id;
+    this.nombre = nombre;
+    }
+    
+    
+    
+}
